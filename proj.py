9b233241e02628f41c8597cd6f6286b713e58009
Kousthubh Yadavalli <kyadavalli04@gmail.com>
2023-04-05 20:13:42 +0530

    car detector

--- proj.py
+++ proj.py
@@ -1,38 +1,22 @@
 import cv2
-import numpy as np
 
-# Load the input image
-# image = cv2.imread('image1-1.png')
-# image = cv2.imread('cars.jpg')
-image = cv2.imread('pen.jpeg')
+# Load the image
+img = cv2.imread('cars.jpeg')
 
-# Convert the input image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Trained XML classifiers describes some features of some object we want to detect
+car_cascade = cv2.CascadeClassifier('carx.xml')
 
-# Apply a Gaussian blur to reduce noise
-gray_blur = cv2.GaussianBlur(gray, (5, 5), 0)
+# Convert the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect edges using Canny edge detection
-edges = cv2.Canny(gray_blur, 50, 150, apertureSize=3)
+# Detect cars in the image
+cars = car_cascade.detectMultiScale(gray, 1.1, 1)
 
-# Find contours of the edges
-contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Draw rectangles around the detected cars
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-# Loop through the contours and find the square shape
-for contour in contours:
-    # Calculate the perimeter of the contour
-    perimeter = cv2.arcLength(contour, True)
-    
-    # Approximate the shape of the contour with a polygon
-    approx = cv2.approxPolyDP(contour, 0.03 * perimeter, True)
-    
-    # Check if the polygon has four sides (i.e. a square)
-    if len(approx) == 4:
-        # Draw a rectangle around the square
-        x, y, w, h = cv2.boundingRect(approx)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-# Display the input and output images
-cv2.imshow('Input', image)
+# Display the image
+cv2.imshow('Cars', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
